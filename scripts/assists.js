ec12f2887af8866a7cb46feaa37249ca4742170e
Mauricio Salinas <kyte.malasky@gmail.com>
2014-10-20 05:16:33 +0000

    Prototype ready

--- scripts/assists.js
+++ scripts/assists.js
@@ -1,28 +1,39 @@
 var Launcher = {
-    CreateFairy: function(game) {
-        
-        var fairy = game.add.sprite(game.world.centerX, game.world.height - 120, 'butterflies');
-        
+    CreateFairy: function(game, altura, fairyGroup) {
+
+        /* X Position random */
+        var xPosition = Math.floor(Math.random() * 496)+10;
+
+        var fairy = game.add.sprite(
+                    xPosition,
+                    altura,
+                    'butterflies'
+        );
+
         /* tamano escala */
         fairy.scale.setTo(2,2);
-        
-        /* Red Buttlerfly */
-        fairy.animations.add('idle', [0, 1, 2], 12, true);
-        
-        /* Yellow Butterfly */
-        fairy.animations.add('idle', [3, 4, 5], 12, true);
-        
-        /* Brown Butterfly */
-        fairy.animations.add('idle', [6, 7, 8], 12, true);
-        
-        /* Pink Butterfly */
-        fairy.animations.add('idle', [9, 10, 11], 12, true);
-        
-        
+
+        /* Random Color */
+        var spriteSelect = Math.floor(Math.random() * 4);
+        switch(spriteSelect){
+            case 0: /* Red Buttlerfly */
+                fairy.animations.add('idle', [0, 1, 2], 12, true);
+                break;
+            case 1: /* Yellow Butterfly */
+                fairy.animations.add('idle', [3, 4, 5], 12, true);
+                break;
+            case 2: /* Brown Butterfly */
+                fairy.animations.add('idle', [6, 7, 8], 12, true);
+                break;
+            case 3: /* Pink Butterfly */
+                fairy.animations.add('idle', [9, 10, 11], 12, true);
+                break;
+        }
+
         // Animaciones
         fairy.animations.play('idle');
-        
-        
-        return fairy;
+
+        // Agregar al grupo
+        fairyGroup.add(fairy);
     }
 };
